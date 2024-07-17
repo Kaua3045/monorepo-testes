d3c1611b11fb29c359d9c1d7576311768e562456
Kauã Pereira <zkauacodes@hotmail.com>
2024-07-17 13:57:43 -0300

    add order tests

---
+++ order-command-ms/src/test/java/com/kaua/template/infrastructure/SampleTest.java
@@ -0,0 +1,14 @@
+package com.kaua.template.infrastructure;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SampleTest {
+
+    @Test
+    void testSum() {
+        Assertions.assertEquals(2, 1 + 1);
+        Assertions.assertEquals(4, 2 + 2);
+        Assertions.assertEquals(6, 3 + 3);
+    }
+}
