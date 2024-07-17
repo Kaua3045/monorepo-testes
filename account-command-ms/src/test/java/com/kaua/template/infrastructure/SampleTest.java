12a55d8d36e0d3391a70b0c78694b51be880f34a
Kauã Pereira <zkauacodes@hotmail.com>
2024-07-17 09:31:35 -0300

    add first test in account command ms

---
+++ account-command-ms/src/test/java/com/kaua/template/infrastructure/SampleTest.java
@@ -0,0 +1,12 @@
+package com.kaua.template.infrastructure;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SampleTest {
+
+    @Test
+    void testSum() {
+        Assertions.assertEquals(2, 1 + 1);
+    }
+}
